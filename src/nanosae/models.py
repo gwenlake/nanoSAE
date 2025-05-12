2ffccd0353521600b34970aaed9e5d1e3e6397b6
Sylvain Barthélémy <sylbarth@gmail.com>
2025-05-12 11:12:37 +0200

    Remove SAETopK

--- src/nanosae/models.py
+++ src/nanosae/models.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 
 class SAE(nn.Module):
     """
-    The autoencoder architecture and initialization used in
+    The SAE architecture and initialization used in
     https://transformer-circuits.pub/2024/april-update/index.html#training-saes
     """
 
@@ -61,83 +61,3 @@ class SAE(nn.Module):
             model.to(device)
         return model
 
-
-class SAETopK(nn.Module):
-    """
-    The autoencoder architecture and initialization used in
-    https://transformer-circuits.pub/2024/april-update/index.html#training-saes
-    adding a topk selection activation
-    """
-
-    def __init__(self, input_size: int, hidden_size: int):
-        super().__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.encoder = nn.Linear(input_size, hidden_size, bias=True)
-        self.decoder = nn.Linear(hidden_size, input_size, bias=True)
-        self.k = 16
-        self.b_dec = nn.Parameter(torch.zeros(input_size))
-        self.threshold = -float('inf')
-
-        # initialize encoder and decoder weights
-        w = torch.randn(input_size, hidden_size)
-        ## normalize columns of w
-        w = w / w.norm(dim=0, keepdim=True) * 0.1
-        ## set encoder and decoder weights
-        self.encoder.weight = nn.Parameter(w.clone().T)
-        self.decoder.weight = nn.Parameter(w.clone())
-
-        # initialize biases to zeros
-        init.zeros_(self.encoder.bias)
-        init.zeros_(self.decoder.bias)
-
-    def encode(self, x: torch.Tensor, return_topk: bool = False, use_threshold: bool = False):
-        post_relu_feat_acts_BF = nn.functional.relu(self.encoder(x - self.b_dec))
-
-        if use_threshold:
-            encoded_acts_BF = post_relu_feat_acts_BF * (post_relu_feat_acts_BF > self.threshold)
-            if return_topk:
-                post_topk = post_relu_feat_acts_BF.topk(self.k, sorted=False, dim=-1)
-                return encoded_acts_BF, post_topk.values, post_topk.indices, post_relu_feat_acts_BF
-            else:
-                return encoded_acts_BF
-
-        post_topk = post_relu_feat_acts_BF.topk(self.k, sorted=False, dim=-1)
-
-        # We can't split immediately due to nnsight
-        tops_acts_BK = post_topk.values
-        top_indices_BK = post_topk.indices
-
-        buffer_BF = torch.zeros_like(post_relu_feat_acts_BF)
-        encoded_acts_BF = buffer_BF.scatter_(dim=-1, index=top_indices_BK, src=tops_acts_BK)
-
-        if return_topk:
-            return encoded_acts_BF, tops_acts_BK, top_indices_BK, post_relu_feat_acts_BF
-        else:
-            return encoded_acts_BF
-
-    def decode(self, f):
-        return self.decoder(f)
-    
-    def forward(self, x, output_features=False):
-        """
-        Forward pass of an autoencoder.
-        x : activations to be autoencoded
-        """
-        if not output_features:
-            return self.decode(self.encode(x))
-        else:  # TODO rewrite so that x_hat depends on f
-            f = self.encode(x)
-            x_hat = self.decode(f)
-            # multiply f by decoder column norms
-            f = f * self.decoder.weight.norm(dim=0, keepdim=True)
-            return x_hat, f
-
-    def from_pretrained(path, device=None):
-        """
-        Load a pretrained autoencoder from a file.
-        """
-        model = torch.load(path, weights_only=False)
-        if device is not None:
-            model.to(device)
-        return model
