f08c426464f244b224083547e4f4579da7bf600f
Guillaume BEGUEC <guillaume.beguec@gwenlake.com>
2025-05-15 11:55:03 +0200

    Add Token Embeddings output

--- src/nanosae/embeddings.py
+++ src/nanosae/embeddings.py
@@ -8,14 +8,21 @@ class Embeddings:
         self.model = SentenceTransformer(model)
         if torch.cuda.is_available():
             self.model = self.model.cuda()
+        self.tokenizer = self.model.tokenizer
 
-    def _embed(self, text: str, prefix: str = None, normalize_embeddings: bool = True):
+    def _embed(self, text: str, prefix: str = None, normalize_embeddings: bool = True, output_value: str = None):
         if prefix:
             text = prefix + text
+        if output_value == "token_embeddings":
+            return [list(e) for e in self.model.encode(text, output_value=output_value)]
         return self.model.encode(text, normalize_embeddings=normalize_embeddings).tolist()
 
-    def embed(self, input_text: list[str], prefix: str = None, normalize_embeddings: bool = True):
+    def embed(self, input_text: list[str], prefix: str = None, normalize_embeddings: bool = True, output_value: str = None):
         embeddings = []
+        if output_value == "token_embeddings":
+            for text in input_text:
+                embeddings += self._embed(text, prefix=prefix, output_value=output_value)
+            return embeddings
         if len(input_text)==1:
             text = input_text[0]
             embeddings.append(self._embed(text, prefix=prefix, normalize_embeddings=normalize_embeddings))
@@ -23,3 +30,6 @@ class Embeddings:
             for text in tqdm(input_text):
                 embeddings.append(self._embed(text, prefix=prefix, normalize_embeddings=normalize_embeddings))
         return embeddings
+    
+    def get_tokens(self, text: str):
+        return self.tokenizer.tokenize(text)
