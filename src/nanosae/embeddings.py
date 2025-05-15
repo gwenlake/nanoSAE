541fa470359c2327b73cf72d258d5c4cdf7679cb
Sylvain Barthélémy <sylbarth@gmail.com>
2025-05-15 15:49:02 +0200

    Update embeddings.py

--- src/nanosae/embeddings.py
+++ src/nanosae/embeddings.py
@@ -9,9 +9,12 @@ class Embeddings:
 
     def __init__(self, model: str):
         self.model = SentenceTransformer(model)
+        self.max_seq_length = self.model.get_max_seq_length()
+        self.tokenizer = self.model.tokenizer
+
         if torch.cuda.is_available():
             self.model = self.model.cuda()
-        self.tokenizer = self.model.tokenizer
+
 
     def _embed(self, text: str, prefix: str = None, normalize_embeddings: bool = True, output_value: str = None):
         if prefix:
@@ -36,21 +39,39 @@ class Embeddings:
             return_attention_mask=False
         )
         ids = output["input_ids"]
+        
         return self.tokenizer.convert_ids_to_tokens(ids)
 
-    def get_token_df(self, input_text: list[str], prefix: str = None, normalize_embeddings: bool = True):
+    def get_token_df(self, input_text: list[str], prefix: str = None):
         data = []
+
+        prefix_num_tokens = 0
+        if prefix:
+            prefix_num_tokens = len(self.get_tokens(prefix))
+
         for text in tqdm(input_text):
-            embeddings = self._embed(text, prefix=prefix, normalize_embeddings=normalize_embeddings, output_value="token_embeddings")
+
+            embeddings = self._embed(text, prefix=prefix, output_value="token_embeddings")
             tokens = self.get_tokens(text, prefix=prefix)
-            for i, token in enumerate(tokens):
-                if prefix and i < 4:
-                    continue
-                if token not in EXCLUDED_TOKENS:
-                    data.append({
-                        "token": token,
-                        "pos": i,
-                        "context": text,
-                        "embeddings": embeddings[i],
-                    })
+
+            if len(tokens) == len(embeddings):
+
+                for i, token in enumerate(tokens[:self.max_seq_length]):
+
+                    if prefix and i < (prefix_num_tokens - 1):
+                        continue
+                    if prefix and i == (prefix_num_tokens - 1):
+                        token = token.lstrip() # remove whitespace on first token if prefix
+
+                    if token not in EXCLUDED_TOKENS:
+                        data.append({
+                            "token": token,
+                            "pos": i,
+                            "context": text,
+                            "embeddings": embeddings[i],
+                        })
+
+            else:
+                print("Tokens missmatch:", len(tokens), "tokens", len(embeddings), "embeddings")
+
         return pd.DataFrame(data)
